11a01886af5a496cfb2df16da94553b14d8677b2
Aakarsh <aakarshsharma664@gmail.com>
2026-06-01 00:01:30 +0530

    Setup PostgreSQL and SQLAlchemy

---
+++ models.py
@@ -0,0 +1,18 @@
+from database import Base
+from sqlalchemy import  Column, Integer, String, TIMESTAMP, Boolean, text
+
+class Application(Base):
+    __tablename__ = "applications"
+
+    id = Column(Integer,primary_key = True, nullable = False)
+    company_name = Column(String, nullable = False)
+    position = Column(String, nullable = False)
+    status = Column(String, nullable = False)
+    application_link = Column(String)
+    notes = Column(String)
+    created_at = Column(TIMESTAMP(timezone = True),server_default = text('now()'))
+    
+
+
+    
+
